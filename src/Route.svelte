36ba6595993d9c326fbd9e827b2e0a3ecd5db0b7
Noah Salvi <noah.salvi@ti8m.ch>
2021-02-12 15:57:21 +0100

    fix bug

--- src/Route.svelte
+++ src/Route.svelte
@@ -102,6 +102,7 @@
 	$: isPreloadPlaceholder =
 		$previousRoute &&
 		$previousRoute.id === id &&
+		$activeRoute &&
 		!$activeRoute.isComponentLoaded;
 
 	// $: console.log("previous", $previousRoute);
